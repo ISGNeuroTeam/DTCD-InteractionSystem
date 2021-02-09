9314db6fb26a4c0f639a161ab39aecccc184af06
romankuramshin <rkuramshin@isgneuro.com>
2021-02-09 11:59:40 +0300

    init-system

---
+++ InteractionSystem/src/InteractionSystem.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { SystemPlugin } from '../../../DTCD-SDK/index';
+
+export class InteractionSystem extends SystemPlugin {
+  static getRegistrationMeta() {
+    return {
+      type: 'core',
+      title: 'Система взаимодействия',
+      name: 'InteractionSystem',
+    };
+  }
+
+  constructor() {
+    super();
+    this.baseURL = window.location.origin;
+    this.instance = axios.create({
+      baseURL: this.baseURL,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+    });
+    const config = {};
+    Object.assign(this, this.instance, config);
+  }
+
+  async GETRequest(url, config = {}) {
+    const response = await this.instance.get(url, config);
+    return response;
+  }
+
+  async POSTRequest(url, data, config = {}) {
+    const response = await this.instance.post(url, data, config);
+    return response;
+  }
+
+  async PUTRequest(url, data, config = {}) {
+    const response = await this.instance.put(url, data, config);
+    return response;
+  }
+
+  async DELETERequest(url, data, config = {}) {
+    const response = await this.instance.delete(url, data, config);
+    return response;
+  }
+}
